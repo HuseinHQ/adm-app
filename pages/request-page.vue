2a5cd11532ed7dec5deebc9a155f36718b99ca39
SyukurIman <ataiman30@gmail.com>
2023-02-28 09:18:55 +0700

    Clean File Request ADN

--- pages/request-page.vue
+++ pages/request-page.vue
@@ -126,14 +126,12 @@
         </div>
       </div>
 
-      <!-- <RequestPagesInputForm />  -->
     </div>
     
   </template>
   
   <script>
     import Navbar from '../components/Navbar.vue';
-    import RequestPagesInputForm from '../components/RequestPagesInputForm.vue';
     export default {
       name: 'RequestPage',
       data() {
@@ -152,8 +150,7 @@
         }
       },
       components: {
-        Navbar,
-        RequestPagesInputForm 
+        Navbar
       },
       methods: {
         activateOptions(){
